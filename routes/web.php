584d1b6b35aa028556480a14cb213c787e2d3c03
Ricardo Silva <sljricardo@gmail.com>
2020-03-21 15:21:40 +0000

    refactoring with query builder

--- routes/web.php
+++ routes/web.php
@@ -9,27 +9,26 @@ $router->get('/', function () {
 
 $router->get('/selledtracks', function() {
     // get best selledtracks from DB
-
-    return DB::select(' SELECT invoice_items.TrackId, count(invoice_items.TrackId) AS total,  tracks.Name track, artists.Name artist
-                        FROM invoice_items 
-                        JOIN tracks ON tracks.TrackId = invoice_items.TrackId 
-                        JOIN albums ON tracks.AlbumId = albums.AlbumId
-                        JOIN artists ON artists.ArtistId = albums.ArtistId 
-                        GROUP BY invoice_items.TrackId 
-                        ORDER BY total DESC LIMIT 5');
+    return DB::table('invoice_items')
+        ->select('invoice_items.TrackId', DB::raw('count(invoice_items.TrackId) AS total'), 'tracks.Name as track', 'artists.Name as artist')
+        ->join('tracks', 'tracks.TrackId', '=', 'invoice_items.TrackId')
+        ->join('albums', 'tracks.AlbumId', '=', 'albums.AlbumId')
+        ->join('artists', 'artists.ArtistId', '=', 'albums.ArtistId')
+        ->groupBy('invoice_items.TrackId')
+        ->orderByDesc('total')
+        ->limit(5)
+        ->get();
 
 });
 
-
 $router->get('/bestcostumer', function() {
     // get best selledtracks from DB
-
-    return DB::select(' SELECT invoices.CustomerId, sum(invoices.Total) AS euros, customers.FirstName, customers.LastName, customers.Email
-                        FROM invoices 
-                        JOIN customers
-                        ON customers.CustomerId = invoices.CustomerId
-                        GROUP BY invoices.CustomerId
-                        ORDER BY total DESC');
+    return DB::table('invoices')
+            ->select('invoices.CustomerId', DB::raw('SUM(invoices.Total) as euros'), 'customers.FirstName', 'customers.LastName', 'customers.Email')
+            ->join('customers', 'customers.CustomerId', '=', 'invoices.CustomerId')
+            ->groupBy('invoices.CustomerId')
+            ->orderByDesc('euros')
+            ->get();
 
 });
 
